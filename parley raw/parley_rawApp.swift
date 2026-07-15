50931327d183a723dfedbb6a2dda7e2e32b2eb77
Yelzhan Yelikbayev <yelzhan.yelikbayev@avbinvest.co>
2026-07-15 15:10:06 +0200

    blank version

---
+++ parley raw/parley_rawApp.swift
@@ -0,0 +1,17 @@
+//
+//  parley_rawApp.swift
+//  parley raw
+//
+//  Created by Yelzhan Yelikbaev on 08.07.2026.
+//
+
+import SwiftUI
+
+@main
+struct parley_rawApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
